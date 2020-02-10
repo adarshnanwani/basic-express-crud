3079552bbef41988eaad3f1ace0b6061bc25c533
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-02-11 00:15:24 +0530

    Add cors package.

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv/config");
 
 const app = express();
 
-// Middleware
+// Middlewares
+app.use(cors());
 app.use(express.json());
 
 // Import routes
